2a8dfeeeaf1aa23e480bba5a3ff2a5067c19a3d6
chris.ag.spencer@gmail.com <chris.ag.spencer@gmail.com>
2024-11-11 19:11:16 +0000

    Refactoring, mutex based GET/SET

--- app/protocol.go
+++ app/protocol.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 // EncodeString encodes a simple string in Redis protocol
@@ -42,23 +43,37 @@ func ReadLine(reader *bufio.Reader) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return line[:len(line)-2], nil // Remove \r\n
+	// Remove trailing \r if present
+	// Conditional to support inline commands
+	return strings.TrimRight(line, "\r\n"), nil
+}
+
+// Parse a BulkString or Array length line
+func parseLengthLine(reader *bufio.Reader, pattern string) (int, error) {
+	lengthLine, err := ReadLine(reader)
+	if err != nil {
+		return 0, err
+	}
+	var length int
+	_, err = fmt.Sscanf(lengthLine, pattern, &length)
+	if err != nil {
+		return 0, err
+	}
+
+	return length, nil
 }
 
 // ReadBulkString reads a bulk string from the reader
 func ReadBulkString(reader *bufio.Reader) (string, error) {
-	lengthLine, err := ReadLine(reader)
-	if err != nil {
-		return "", err
-	}
-	var length int
-	_, err = fmt.Sscanf(lengthLine, "$%d", &length)
+	length, err := parseLengthLine(reader, "$%d")
 	if err != nil {
 		return "", err
 	}
 	if length == -1 {
 		return "", nil // Null bulk string
 	}
+
+	// Read the contents line, sizing the buffer to the declared length.
 	buf := make([]byte, length+2)
 	_, err = reader.Read(buf)
 	if err != nil {
@@ -69,18 +84,15 @@ func ReadBulkString(reader *bufio.Reader) (string, error) {
 
 // ReadArray reads an array from the reader
 func ReadArray(reader *bufio.Reader) ([]string, error) {
-	lengthLine, err := ReadLine(reader)
-	if err != nil {
-		return nil, err
-	}
-	var length int
-	_, err = fmt.Sscanf(lengthLine, "*%d", &length)
+	length, err := parseLengthLine(reader, "*%d")
 	if err != nil {
 		return nil, err
 	}
 	if length == -1 {
 		return nil, nil // Null array
 	}
+
+	// Read number of BulkStrings as declared
 	values := make([]string, length)
 	for i := 0; i < length; i++ {
 		value, err := ReadBulkString(reader)
--- app/server.go
+++ app/server.go
@@ -8,11 +8,22 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
+
+type ServerState struct {
+	mutex  sync.RWMutex
+	values map[string]string
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here.")
+
+	state := ServerState{
+		mutex:  sync.RWMutex{},
+		values: map[string]string{},
+	}
 
 	// Bind to port
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -28,11 +39,23 @@ func main() {
 			os.Exit(1)
 		}
 
-		go handleConnection(conn)
+		go handleConnection(conn, &state)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func checkReadError(err error, addr string) bool {
+	if err == io.EOF {
+		log.Printf("[%s] Disconnected", addr)
+		return true
+	} else if err != nil {
+		log.Printf("[%s] Got error reading client command `%#v`", addr, err)
+		return true
+	}
+
+	return false
+}
+
+func handleConnection(conn net.Conn, state *ServerState) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
@@ -41,14 +64,32 @@ func handleConnection(conn net.Conn) {
 	for {
 		// For now, assume command is sent as a Bulk String
 
-		rawCommand, err := ReadArray(reader)
-		if err == io.EOF {
-			log.Printf("[%s] Disconnected", addr)
+		// Peek the first character to determine the datatype being sent
+		c, err := reader.Peek(1)
+
+		if checkReadError(err, addr) {
 			return
 		}
-		if err != nil {
-			log.Printf("[%s] Got error reading client command `%#v`", addr, err)
-			return
+
+		var rawCommand = []string{}
+
+		switch string(c) {
+		// Read standard Array commands format
+		case "*":
+			rawCommand, err = ReadArray(reader)
+
+			if checkReadError(err, addr) {
+				return
+			}
+		// Fallback to inline commands format
+		default:
+			rawLine, err := ReadLine(reader)
+
+			if checkReadError(err, addr) {
+				return
+			}
+
+			rawCommand = strings.Split(rawLine, " ")
 		}
 
 		command := strings.ToUpper(rawCommand[0])
@@ -57,14 +98,46 @@ func handleConnection(conn net.Conn) {
 		switch command {
 		case "PING":
 			conn.Write([]byte(EncodeString("PONG")))
+
 		case "ECHO":
 			if len(rawCommand) != 2 {
 				log.Printf("[%s] Malformed ECHO request: %#v", addr, rawCommand)
-				return
+				conn.Write([]byte(EncodeError("Malformed request.")))
+				continue
 			}
 
 			// Format as Bulk String and return response.
 			conn.Write([]byte(EncodeBulkString(rawCommand[1])))
+
+		case "SET":
+			if len(rawCommand) != 3 {
+				log.Printf("[%s] Malformed SET request: %#v", addr, rawCommand)
+				conn.Write([]byte(EncodeError("Malformed request.")))
+				continue
+			}
+
+			state.mutex.Lock()
+			state.values[rawCommand[1]] = rawCommand[2]
+			state.mutex.Unlock()
+
+			conn.Write([]byte(EncodeString("OK")))
+
+		case "GET":
+			if len(rawCommand) != 2 {
+				log.Printf("[%s] Malformed GET request: %#v", addr, rawCommand)
+				conn.Write([]byte(EncodeError("Malformed request.")))
+				continue
+			}
+
+			state.mutex.RLock()
+			value, exists := state.values[rawCommand[1]]
+			state.mutex.RUnlock()
+
+			if exists {
+				conn.Write([]byte(EncodeBulkString(value)))
+			} else {
+				conn.Write([]byte(EncodeBulkString("nil")))
+			}
 
 		default:
 			log.Printf("[%s] Unknown command '%s'", addr, command)
